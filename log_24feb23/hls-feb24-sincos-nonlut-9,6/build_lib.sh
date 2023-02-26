612251d513d3add797da64a91d9d176fda45f0d8
Ho-Fung Tsoi <hftsoi0411@gmail.com>
2023-02-26 16:25:11 -0500

    add vivado_synth

--- log_24feb23/hls-feb24-sincos-nonlut-9,6/build_lib.sh
+++ log_24feb23/hls-feb24-sincos-nonlut-9,6/build_lib.sh
@@ -10,7 +10,7 @@ HLS_LIBS_PATH=/data/tools/Xilinx/Vivado/2020.1/lnx64
 LDFLAGS="-Wl,--no-undefined -Wl,--no-allow-shlib-undefined -Wl,--no-as-needed -Wl,-rpath,${HLS_LIBS_PATH}/lib/csim -L ${HLS_LIBS_PATH}/lib/csim -lhlsmc++-GCC46 -lhlsm-GCC46 -fno-builtin -fno-inline -Wl,-rpath,${HLS_LIBS_PATH}/tools/fpo_v7_0 -L ${HLS_LIBS_PATH}/tools/fpo_v7_0 -lgmp -lmpfr -lIp_floating_point_v7_0_bitacc_cmodel"
 INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
-LIB_STAMP=Db0dCdFA
+LIB_STAMP=AB2a69E8
 
 ${CC} ${CFLAGS} ${INCFLAGS} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
 ${CC} ${CFLAGS} ${INCFLAGS} -c ${PROJECT}_bridge.cpp -o ${PROJECT}_bridge.o
